eb3fabc0d26f2d39d762eef3f08f177c51593b5f
Raghav Kapoor <raghavkapoor@Raghavs-MacBook-Air.local>
2020-10-18 18:24:40 +0530

    ADDED THE FUNCTIONALITY TO CALCULATE THE VALUE OF EVERY CHROMOSOMES BASED ON THEIR RANDOM SEQUENCES

--- evpop.c
+++ evpop.c
@@ -3,7 +3,7 @@
 
 struct Chrom
 {
-    int arr[6];
+    int bit[6];
     int fit;
 }chrom;
 void evpop(struct Chrom population[]);
@@ -19,13 +19,14 @@ void evpop(struct Chrom population[])
     int random;
     int i=0;
     int k=0;
+    int value;
     for(int j=0;j<4;j++)
     {
         for(int i=0;i<6;i++)
         {
             random=rand();
             random=random%2;
-            population[j].arr[i]=random;
+            population[j].bit[i]=random;
         }
         
     }
@@ -34,12 +35,21 @@ void evpop(struct Chrom population[])
         printf("Chromosome %d\t",j+1);
         for(int i=0;i<6;i++)
         {
-            printf("%d",population[j].arr[i]);
+            printf("%d",population[j].bit[i]);
         }
         printf("\n");
         
     }
     
-    
-    
+for(j=0;j<4;j++)
+    {
+    value=getvalue(population[j]);
+    printf("Chromosome %d\t%d\n",j+1,value);
+    }
+}
+int getvalue(struct Chrom human)
+{
+    int value;
+    value=(human.bit[5]*1)+(human.bit[4]*2)+(human.bit[3]*4)+(human.bit[2]*8)+(human.bit[1]*16)+(human.bit[0]*32);
+    return value;
 }
