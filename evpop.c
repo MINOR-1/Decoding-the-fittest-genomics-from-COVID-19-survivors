b4a6ae50f6d36418b907f17bcb19f2dd0459eb02
Anshul Chaudhary <60177875+anshulgit7@users.noreply.github.com>
2020-10-25 15:42:03 +0530

    added the functionality of cross-over

--- evpop.c
+++ evpop.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Chrom
+typedef struct Chrom             		
 {
-    int bit[6];
+    short int bit[6];
     int fit;
+
 }chrom;
 
-void evpop(struct Chrom population[]);
-void pickchroms(struct Chrom popcurrent[]);
+
+void evpop(chrom popcurrent[5]);
+void pickchroms(chrom popcurrent[5]);
+void crossover(chrom popcurrent[5]);
 int find_fit_value(int value);
+int getvalue(chrom popcurrent);
 
 int main()
 {
-    struct Chrom population[4];
-    evpop(population);
-    pickchroms(population);
+    chrom population[5];
 
-    return 0;
+	evpop(population);
+    
+	pickchroms(population);
+
+	crossover(population);
+    return (0);
 }
-void evpop(struct Chrom population[])
+
+void evpop(chrom population[])
 {
     int j=0;
     int random;
     int i=0;
     int k=0;
     int value;
-    for(int j=0;j<4;j++)
+    for(int j=0;j<5;j++)
     {
         for(int i=0;i<6;i++)
         {
@@ -37,10 +45,10 @@ void evpop(struct Chrom population[])
         
         
     }
-    printf("# HERE IS THE COMPLETE INFORMATION ABOUT THE ENTIRE POPULATION SET OF CHROMOSOMES\n");
-    printf("*********************************************************************************\n");
+    printf("$ INITIAL SET OF CHROMOSOMES:\n");
+    printf("*******************************\n");
 
-    for(j=0;j<4;j++)
+    for(j=0;j<5;j++)
     {
      value=getvalue(population[j]);
      population[j].fit=find_fit_value(value);
@@ -54,19 +62,20 @@ void evpop(struct Chrom population[])
         }
         printf("     ");
         printf("value=%d\t",value);
-        printf("fit_value=%d" ,population[j].fit);
-        printf("\n");
+        printf("fitness=%d" ,population[j].fit);
         printf("\n");
 
 
     }
+    printf("*******************************\n\n\n\n\n");
+
 
 }
 
-int getvalue(struct Chrom human)
+int getvalue(chrom population)
 {
     int value;
-    value=(human.bit[5]*1)+(human.bit[4]*2)+(human.bit[3]*4)+(human.bit[2]*8)+(human.bit[1]*16)+(human.bit[0]*32);
+    value=(population.bit[5]*1)+(population.bit[4]*2)+(population.bit[3]*4)+(population.bit[2]*8)+(population.bit[1]*16)+(population.bit[0]*32);
     return value;
 }
 
@@ -76,7 +85,8 @@ int find_fit_value(int value)
     int fit_value=(value*value)+random;
     return fit_value;
 }
-void pickchroms(struct Chrom popcurrent[])
+
+void pickchroms(chrom population[5])
 {
 
  int i,j;
@@ -85,22 +95,60 @@ void pickchroms(struct Chrom popcurrent[])
     for(i=0;i<3;i++)
        for(j=0;j<3;j++)
          {
-             if(popcurrent[j+1].fit>popcurrent[j].fit)
+             if(population[j+1].fit>population[j].fit)
                {
-                 temp=popcurrent[j+1];
-                 popcurrent[j+1]=popcurrent[j];
-                 popcurrent[j]=temp;
+                 temp=population[j+1];
+                 population[j+1]=population[j];
+                 population[j]=temp;
 
                }
          }
-        printf("# Performing the required sorting opertion in order to achieve the best chromosomes.................%\n");
-        printf("# After Performing The sorting on the chromoses we ll get the reuired sequence in the descending order of fit_value\n");
-        printf("*******************************************************************************************************************\n\n");
+        printf("$ PERFORMING SORTING ON INITIAL SET OF CHROMOSOMES.........%\n");
+        printf("$ AFTER SORTING:\n");
+    printf("*******************************\n");
 
-    for(i=0;i<4;i++)
+    for(i=0;i<5;i++)
     {
-        printf("chromoses=%d\t fitness=%d",i,popcurrent[i].fit);
+        printf("chromoses=%d\t fitness=%d",i+1,population[i].fit);
         printf("\n");
     }
-  return(0);
+    printf("*******************************\n\n\n\n\n");
+
+}
+
+void crossover(chrom population[5])
+{
+	int i;
+	int random_number;
+	
+	random_number = rand();		
+	
+	random_number = ((random_number%5)+1);	
+	for(i=0;i<random_number;i++)	
+	{
+		population[3].bit[i]=population[2].bit[i];	
+        population[4].bit[i]=population[1].bit[i];	
+	}
+	
+	for(i=random_number;i<6;i++)
+	{
+		population[3].bit[i]=population[1].bit[i];	
+        population[4].bit[i]=population[2].bit[i];	
+	}
+	
+	for(i=0;i<5;i++)	
+    	population[i].fit=find_fit_value(getvalue(population[i]));	
+	printf("$ PERFORMING CROSSOVER ON SORTED SET OF CHROMOSOMES.........%\n");
+	printf("$ AFTER CROSSOVER:\n");
+	printf("*******************************");
+
+
+	
+	
+    for(i=0;i<5;i++)                           
+    	printf("\nChromosome %d\nSequence=%d%d%d%d%d%d\t    value=%d\t fitness = %d",i+1,population[i].bit[5],population[i].bit[4],population[i].bit[3],population[i].bit[2],population[i].bit[1],population[i].bit[0],getvalue(population[i]),population[i].fit);
+        		
+	printf("\n*********************************");
+
+		
 }
